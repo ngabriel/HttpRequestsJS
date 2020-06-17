48b2e40493914d772d4ef7fe1d6f9abbaea92ad8
Naomi Gabriel <naomi_gab@hotmail.com>
2020-06-17 15:42:54 +0100

    retrieve data and display on html

--- requests.js
+++ requests.js
@@ -28,21 +28,72 @@ function fetchData(){
 }
 
 function axiosData(){
-    axios.get("https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json")
+    axios.get("https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/kings.json")
     .then(res => out.innerText = JSON.stringify(res.data, undefined, 4))
     .catch(err => console.log(err))
 }
 
-function axiosDataElemtnts(){
+function axiosData2(){
     axios.get("https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json")
-    .then(res =>{
-        
+    .then(res => out.innerText = JSON.stringify(res.data, undefined, 4))
+    .catch(err => console.log(err))
+}
+function axiosDataElements(){
+    axios.get("https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/kings.json")
+    .then(res => {
+        res.data.forEach(kings => {
+            const dataDiv = document.createElement('div');
+            bigOutput.appendChild(dataDiv);
+
+            const name = document.createElement("h2");
+            name.innerText = kings.nm;
+            dataDiv.appendChild(name);
+
+            const cityT = document.createElement("h3");
+            cityT.innerText = kings.cty;
+            dataDiv.appendChild(cityT);
+
+            const houseT = document.createElement("h4");
+            houseT.innerText = kings.hse;
+            dataDiv.appendChild(houseT);
+
+            const yrsT = document.createElement("h4");
+            yrsT.innerText = kings.yrs;
+            dataDiv.appendChild(yrsT);
+
+            
+        })
     })
     .catch(err => console.log(err))
-    
-
 }
 
+function axiosDataElements2(){
+    axios.get("https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json")
+    .then(res => {
+        res.data.members.forEach(supers => {
+            const dataDiv = document.createElement('div');
+            bigOutput.appendChild(dataDiv); 
+            const name = document.createElement("h2");
+            name.innerText = supers.name;
+            dataDiv.appendChild(name);
+
+            const age2 = document.createElement("h2");
+            age2.innerText = supers.age;
+            dataDiv.appendChild(age2);
+
+            const si = document.createElement("h2");
+            si.innerText = supers.secretIdentity;
+            dataDiv.appendChild(si);
+
+            const pwr = document.createElement("h2");
+            pwr.innerText = supers.powers;
+            dataDiv.appendChild(pwr);
+
+
+        })
+    })
+    .catch(err => console.log(err))
+}
 
 
 
